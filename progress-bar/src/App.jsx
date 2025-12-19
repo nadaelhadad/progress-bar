d702d73dff2381187bb08342ef32988a934faa00
nadaelhadad <nada.elhadad@ejust.edu.eg>
2025-12-19 16:29:47 +0200

    initial commit

---
+++ progress-bar/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [percentage, setpercentage] = useState(10)
+
+  const handleChange=(e=>{
+    let value=parseInt(e.target.value)
+    setpercentage(value)
+  })
+  return (
+    <>
+      <div className='progress-bar'>
+        <div className='progress-filled' style={{width:{percentage}}}>{percentage}%</div>
+      </div>
+      <div className="input-container">
+        <label htmlFor='input'>Input Percentage:</label>
+        <input id='input' name='input' value={percentage} type='number' onChange={handleChange}></input>
+      </div>
+    </>
+  )
+
+}
+
+export default App
